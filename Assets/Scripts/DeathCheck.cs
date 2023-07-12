1857ef66ca34fa86efe11c75e20fe4b8943a278c
Aditya Singh <s.addy1317@gmail.com>
2023-07-12 20:40:43 +0530

    Game Update : - Level Start - Level End

--- Assets/Scripts/DeathCheck.cs
+++ Assets/Scripts/DeathCheck.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace Outscal
 {
-    public class LevelOverController : MonoBehaviour
+    public class DeathCheck : MonoBehaviour
     {
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.gameObject.GetComponent<PlayerController>() != null);
+            if (collision.gameObject.GetComponent<PlayerController>() != null) ;
             {
-                Debug.Log("Level Completed");
+               
+                Debug.Log("Player is Dead");
+                SceneManager.LoadScene(1);
             }
         }
     }
 }
+
